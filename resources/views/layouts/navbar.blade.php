a4f6c6007f9f671f57eb0106684cf04996065fb9
okycelt <ondrejkycelt@aol.com>
2017-02-27 22:22:51 +0100

    basic layout structure

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-light rounded navbar-toggleable-md">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#containerNavbar" aria-controls="containerNavbar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">CSAD</a>
+
+  <div class="collapse navbar-collapse" id="containerNavbar">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pobočky</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Zaměstnanci</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Vozidla</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Jízdní řády</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Zákazníci</a>
+      </li>
+    </ul>
+  </div>
+</nav>
